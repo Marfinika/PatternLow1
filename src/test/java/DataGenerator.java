d8310b1d5542bf716135682dbec10977f98f0f01
Ivan Zuev <ivan.zyev96@yandex.ru>
2023-01-30 18:47:50 +0300

    Initial commit

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,39 @@
+import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static Faker getFaker() {
+        return new Faker(new Locale("ru"));
+    }
+
+    public static LocalDate generateDate() {
+        long minDay = LocalDate.now().plusDays(7).toEpochDay();
+        long maxDay = LocalDate.now().plusMonths(10).toEpochDay();
+        long randomDay = ThreadLocalRandom.current().nextLong(minDay, maxDay);
+        return LocalDate.ofEpochDay(randomDay);
+    }
+    public static String getFormatDate(LocalDate date) {
+        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+
+        return DataGenerator.getFaker().address().city();
+    }
+
+    public static String generateName() {
+
+        return DataGenerator.getFaker().name().fullName();
+    }
+
+    public static String generatePhone() {
+
+        return DataGenerator.getFaker().phoneNumber().phoneNumber();
+    }
+}
